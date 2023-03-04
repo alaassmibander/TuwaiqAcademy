ebb5ebc7b4d1adf5e88c6571133a8095b2ef94fc
alaassmibander <122218991+alaassmibander@users.noreply.github.com>
2023-03-04 22:14:57 +0300

    Create day3ValidationApplication.java

---
+++ Week3/Day 3/src/main/java/com/example/day3/day3ValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3Day4ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3Day4ValidationApplication.class, args);
+    }
+
+}
